c77a9e09c82b45bb6d681869cfc7e0664ac391b9
wangz <wangzhuo1987@gmail.com>
2013-10-09 16:11:38 +0800

    get all

--- obtaindata/getall.py
+++ obtaindata/getall.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#encoding=utf-8
+
 import datetime
-import os
+import os,sys
 
 
 if len(sys.argv)==4:
@@ -8,7 +12,7 @@ if len(sys.argv)==4:
         url_date1 = sys.argv[2]
         url_date2 = sys.argv[3]
 else:
-    print "need 3 args ,which ,time from, and time end ,example:shfe 20120912 20120915"
+    print "need 3 args ,now is %s, which to execute(shfe,dce,cffex or czce) ,time from, and time end ,example:shfe 20120912 20120915" % (len(sys.argv)-1)
     exit(1)
 
 if where == 'shfe':	
@@ -20,11 +24,10 @@ if where == 'cffex':
 if where == 'czce':
     locate = "./obtain_czce.py %s"
 
-d1 = datetime.date(url_date1[0:4],url_date1[4:6],url_date1[6:8])
+d1 = datetime.date(int(url_date1[0:4]),int(url_date1[4:6]),int(url_date1[6:8]))
 
-while(d1 < datetime.date(url_date2[0:4],url_date2[4:6],url_date2[6:8])):
-    d1 = d1 + datetime.timedelta(1)
+while(d1 <= datetime.date(int(url_date2[0:4]),int(url_date2[4:6]),int(url_date2[6:8]))):
     print d1.strftime('%Y%m%d')
     os.system(locate % d1.strftime('%Y%m%d') )
-    
+    d1 = d1 + datetime.timedelta(1)
 
